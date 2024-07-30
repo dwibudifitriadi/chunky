caffe0682ff054049545da6aae748acba99b325d
Dwi Budi Fitri Adi <116490361+dwibudifitriadi@users.noreply.github.com>
2024-07-30 18:54:47 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily:{
+        inter : ['Inter','sans-serif'],
+        dancingscript : ['Dancing Script','sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
